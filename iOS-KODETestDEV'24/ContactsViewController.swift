69aaad1eec3a4560abc56c9085bf202dd1108207
Artem Elchev <kuspus1@gmail.com>
2024-03-22 17:40:26 +0200

    Create Project, delete storyboard, create groups

---
+++ iOS-KODETestDEV'24/ContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  iOS-KODETestDEV'24
+//
+//  Created by Artem Elchev on 22.03.2024.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
