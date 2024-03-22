de7ed7a554748ab16ef68787b80154b66a69d023
Artem Elchev <kuspus1@gmail.com>
2024-03-22 18:21:38 +0200

    Added ContactsTableView

--- iOS-KODETestDEV'24/MainContacts/Views/ContactsTableView.swift
+++ iOS-KODETestDEV'24/MainContacts/Views/ContactsTableView.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+final class ContactsTableView: UITableView {
+
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: .zero, style: .plain)
+        configureTableView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - ContactTableView setup
+    private func configureTableView() {
+        self.showsVerticalScrollIndicator = false
+        self.backgroundColor = .white
+        self.register(ContactTableViewCell.self, forCellReuseIdentifier: "ContactTableViewCell")
+        self.separatorStyle = .none
+    }
+    
+}
