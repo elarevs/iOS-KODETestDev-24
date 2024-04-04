a00913cd296a37b577615cded14ebbbcea5e788a
Artem Elchev <kuspus1@gmail.com>
2024-04-04 10:46:14 +0200

    Bug fixes

---
+++ iOS-KODETestDEV'24/MainContacts/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  iOS-KODETestDEV'24
+//
+//  Created by Artem Elchev on 27.03.2024.
+//
+
+import Foundation
---
+++ iOS-KODETestDEV'24/SortContacts/Enums/TypeOfSorted.swift
@@ -0,0 +1,8 @@
+//
+//  TypeOfSorted.swift
+//  iOS-KODETestDEV'24
+//
+//  Created by Artem Elchev on 30.03.2024.
+//
+
+import Foundation
